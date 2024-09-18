750dc6478836ab25502c4c386b919ec46035a806
Onur45500 <onur.akmese@viacesi.fr>
2024-09-18 15:29:45 +0200

    Implemented the functionnality

--- csharp-print-string-in-reverse/Program.cs
+++ csharp-print-string-in-reverse/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Runtime.CompilerServices;
+
+namespace PrintInReverse
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter empty/nothing to quit.\n");
+            Console.Write("Write something : ");
+
+            string text = Console.ReadLine();
+
+            while(text != string.Empty)
+            {
+                Console.Write("Text reversed : ");
+
+                for (int i = text.Length - 1; i >= 0; i--)
+                {
+                    Console.Write($"{text[i]}");
+                }
+
+                Console.Write("\nWrite something : ");
+                text = Console.ReadLine();
+            }
+            
+        }
+    }
+}
